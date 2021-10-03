b243d959573477ada8df1711b10dc3d47580f322
Na hyunwoo <dusdjeks@naver.com>
2021-10-03 22:37:55 +0900

    fragment album proceeding

--- BottomNaviTemlate/FLO/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ BottomNaviTemlate/FLO/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -1,8 +1,18 @@
 package com.example.flo
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentAlbumBinding
 
 class AlbumFragment : Fragment() {
 
-//    lateinit var binding:FragmentAlbumBinding
+    lateinit var binding: FragmentAlbumBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding
+
+
+    }
 }
